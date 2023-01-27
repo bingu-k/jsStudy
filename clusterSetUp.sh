cc51f28ea871bcfef8c2b52b3287f92fdb594365
Byeunghyeun Kim <byeukim@c6r4s8.42seoul.kr>
2023-01-27 19:37:24 +0900

    first study : Conditional

---
+++ clusterSetUp.sh
@@ -0,0 +1,13 @@
+# install homebrew at goinfre
+cd $HOME/goinfre
+git clone --depth=1 https://github.com/Homebrew/brew.git && echo 'export PATH=$HOME/goinfre/brew/bin:$PATH' >> $HOME/.zshrc && source $HOME/.zshrc
+rm -fr $HOME/Library/Caches
+brew update
+
+#install ncdu
+brew install ncdu
+
+#install node.js && npm
+brew install node
+node --version
+npm --version
